61d3e085b1cefb999de34f425f91925c0aa647ac
leeyeji <yeji1135@gachon.ac.kr>
2025-07-08 02:12:08 +0900

    🎉[Chore] 프로젝트 생성

---
+++ Encorely/Encorely/App/EncorelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncorelyApp.swift
+//  Encorely
+//
+//  Created by 이예지 on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct EncorelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Encorely/Encorely/Modules/Tab/EncorelyTabView.swift
@@ -0,0 +1,18 @@
+//
+//  EncorelyTabView.swift
+//  Encorely
+//
+//  Created by 이예지 on 7/8/25.
+//
+
+import SwiftUI
+
+struct EncorelyTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EncorelyTabView()
+}
